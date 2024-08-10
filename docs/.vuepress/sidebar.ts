521a28512e62cce032dfd4adf2db9c4bde173a3c
KIA <mojtabahosseinikia@gmail.com>
2024-08-10 10:45:53 +0330

    main

---
+++ docs/.vuepress/sidebar.ts
@@ -0,0 +1,105 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  //'/':'structure',
+  '/': [
+    '/get-started/appeditor',
+    {
+      text: 'شروع',
+      //icon: "tip",
+      // optional, link of group title
+      //path: "/get-started/",
+      // optional, will be appended to each item link
+      //prefix: "/foo/",
+      collapsible: true,
+      children: [
+        '/get-started/register',
+        '/get-started/first-app'
+      ]
+    },
+    {
+      text: 'طراحی اپلیکیشن',
+      collapsible: true,
+      //path:'/application-design/page-design',
+      children: [
+        '/application-design/design-environment',
+        '/application-design/page-design',
+        '/application-design/widgets/introduction',
+        //'/application-design/widgets/layout',//به بخش معرفی ویجت منتقل شود
+        //'/application-design/widgets/animation',//به بخش معرفی ویجت منتقل شود
+        {
+          text:'انواع ویجت',
+          collapsible:true,
+          //path:'/application-design/widgets/introduction',
+          children:[
+            /*{text:'#گروه',link:'under-construction'},//'/application-design/widgets/group',
+            {text:'#متن',link:'under-construction'},//'/application-design/widgets/text',
+            {text:'#متن پیشرفته',link:'under-construction'},//'/application-design/widgets/text',
+            {text:'#دکمه',link:'under-construction'},//'/application-design/widgets/button',
+            {text:'#عکس',link:'under-construction'},//'/application-design/widgets/image',
+            {text:'#پخش کننده صوت',link:'under-construction'},//'/application-design/widgets/audio',
+            {text:'#پخش کننده ویدئو',link:'under-construction'},//'/application-design/widgets/video',
+            {text:'#نمایشگر وب(وب ویو)',link:'under-construction'},//'/application-design/widgets/webview',*/
+            '/application-design/widgets/pdf',
+            '/application-design/widgets/tapsell',
+            '/application-design/widgets/rss-reader',
+            '/application-design/widgets/favorites',
+            '/application-design/widgets/tab',
+            '/application-design/widgets/form',
+            '/application-design/widgets/slide',
+          ]
+        },
+
+        '/application-design/menu-design',//انواع منو
+        '/application-design/iab-items',
+        '/application-design/templates',//قالب ها
+        '/application-design/icons',//آیکون ها
+          '/application-design/events',
+          '/application-design/actions',
+          '/application-design/conditions',
+          '/application-design/variables/',
+          '/application-design/javascript',
+      ]
+    },
+    {
+      text: 'سرویس ها',
+      collapsible: true,
+      //collapsed:false,
+      children: [
+        '/services/shop-maker/',//فروشگاه ساز
+        '/services/form-builder',//فرم ساز
+        '/services/account',//حساب کاربری
+        '/services/in-app-purchase',//پرداخت درون برنامه ای
+        '/services/online-content-update',//آپدیت آنلاین محتوا
+        '/services/update-version',//آپدیت آنلاین محتوا
+        '/services/notification',
+        '/services/ads',//تبلیغات
+        '/services/wordpress',
+        '/services/woocommerce',
+      ]
+    },
+    {
+      text: 'ساخت و دریافت خروجی',
+      collapsible: true,
+      //collapsed:false,
+      children: [
+        '/make-application/configs',
+        '/make-application/keystore',
+        '/make-application/source-code',
+        '/make-application/output',
+      ]
+    },
+    {
+      text: 'آموزشهای متفرقه',
+      collapsible: true,
+      //collapsed:false,
+      children: [
+        '/other/how-to-embed-aparat-video',
+        '/other/how-to-turn-a-site-into-an-application',
+      ]
+    },
+    '/introduction',
+    '/changelog',
+    '/FAQ',
+  ]
+});
---
+++ docs/.vuepress/theme.ts
@@ -0,0 +1,201 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import navbar from "./navbar.js";
+import sidebar from "./sidebar.js";
+import {LocaleConfig} from "@vuepress/core";
+import { readingTimePlugin } from "vuepress-plugin-reading-time2";
+
+export default hopeTheme({
+  hostname: "https://help.appeditor.ir",
+
+  /*author: {
+    name: "اپ‌ادیتور",
+    url: "https://appeditor.ir",
+  },*/
+
+  iconAssets: "fontawesome-with-brands",
+
+  logo: "images/logo-70.png",
+
+  //repo: "vuepress-theme-hope/vuepress-theme-hope",
+
+  docsDir: "src",
+  headerDepth:4,
+  //toc:false,
+  metaLocales:{
+    toc:'در این صفحه',
+    readingTime:'مدت مطالعه',
+    prev:'قبلی',
+    next:'بعدی',
+    category:'دسته',
+    tag:'تگ',
+    print:'چاپ',
+  },
+  routeLocales:{
+    back:'برگشت',
+    home:"صفحه اصلی",
+    openInNewWindow:"در پنجره جدید باز شود",
+    notFoundTitle:"صفحه یافت نشد!",
+    notFoundMsg:["دوباره تلاش کن!"]
+  },
+  outlookLocales:{
+    darkmode:'روشنایی'
+  },
+
+  // navbar
+  navbar,
+  // sidebar
+  sidebar,
+  breadcrumb: true,
+  rtl:true,
+  copyright:'کلیه حقوق محفوظ است',
+  //footer: "راهنمای اپ‌ادیتور",
+  displayFooter: true,
+  encrypt: {
+    config: {
+      "/demo/encrypt.html": ["1234"],
+    },
+  },
+
+  /*metaLocales: {
+    editLink: "Edit this page on GitHub",
+  },*/
+  plugins: {
+
+    copyCode: {
+      showInMobile:true
+    },
+    search:true,
+    readingTime:false,
+    // You should generate and use your own comment service
+    /*comment: {
+      provider: "Giscus",
+      repo: "vuepress-theme-hope/giscus-discussions",
+      repoId: "R_kgDOG_Pt2A",
+      category: "Announcements",
+      categoryId: "DIC_kwDOG_Pt2M4COD69",
+    },*/
+
+    // All features are enabled for demo, only preserve features you need here
+    mdEnhance: {
+      align: true,
+      attrs: true,
+      codetabs: true,
+      component: true,
+      demo: true,
+      figure: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      mark: true,
+      /*stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],*/
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+
+      // install chart.js before enabling it
+      // chart: true,
+
+      // insert component easily
+
+      // install echarts before enabling it
+      // echarts: true,
+
+      // install flowchart.ts before enabling it
+      // flowchart: true,
+
+      // gfm requires mathjax-full to provide tex support
+      // gfm: true,
+
+      // install katex before enabling it
+      // katex: true,
+
+      // install mathjax-full before enabling it
+      // mathjax: true,
+
+      // install mermaid before enabling it
+      // mermaid: true,
+
+      // playground: {
+      //   presets: ["ts", "vue"],
+      // },
+
+      // install reveal.js before enabling it
+      // revealJs: {
+      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+      // },
+
+      // install @vue/repl before enabling it
+      // vuePlayground: true,
+    },
+
+    // uncomment these if you want a pwa
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cachePic: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+});
